812ce6f002561f6a1edc0775548a928787f8efb7
yurim <y1551463@gmail.com>
2025-06-11 21:05:01 +0900

    Initial commit

---
+++ src/main/java/io/github/Yuurim98/mojip_go/MojipGoApplication.java
@@ -0,0 +1,13 @@
+package io.github.Yuurim98.mojip_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojipGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojipGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Yuurim98/mojip_go/MojipGoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Yuurim98.mojip_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojipGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
