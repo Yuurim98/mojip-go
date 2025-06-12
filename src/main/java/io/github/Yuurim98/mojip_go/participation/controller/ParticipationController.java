22ea96e3220c3ccdff2aab861335b521eef46e9b
yurim <y1551463@gmail.com>
2025-06-12 13:52:55 +0900

    Feat: 참여 패키지 구조

---
+++ src/main/java/io/github/Yuurim98/mojip_go/participation/controller/ParticipationController.java
@@ -0,0 +1,5 @@
+package io.github.Yuurim98.mojip_go.participation.controller;
+
+public class ParticipationController {
+
+}
---
+++ src/main/java/io/github/Yuurim98/mojip_go/participation/service/ParticipationService.java
@@ -0,0 +1,5 @@
+package io.github.Yuurim98.mojip_go.participation.service;
+
+public class ParticipationService {
+
+}
