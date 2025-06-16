38569c282c1478d3da5d044babb702e8f7ff1afb
yurim <y1551463@gmail.com>
2025-06-16 14:48:28 +0900

    Refactor: ResponseEntity 제외 및 기존 세션 무효화 처리

--- src/main/java/io/github/Yuurim98/mojip_go/auth/controller/AuthController.java
+++ src/main/java/io/github/Yuurim98/mojip_go/auth/controller/AuthController.java
@@ -1,12 +1,13 @@
 package io.github.Yuurim98.mojip_go.auth.controller;
 
 import io.github.Yuurim98.mojip_go.auth.dto.LoginReqDto;
+import io.github.Yuurim98.mojip_go.auth.dto.SessionDto;
 import io.github.Yuurim98.mojip_go.auth.service.AuthService;
 import io.github.Yuurim98.mojip_go.common.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,23 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@Valid @RequestBody final LoginReqDto loginReqDto, HttpSession session) {
-        Long userId = authService.authenticate(loginReqDto.getEmail(), loginReqDto.getPassword());
+    public ApiResponse<String> login(@Valid @RequestBody final LoginReqDto loginReqDto,
+        HttpServletRequest request) {
 
-        session.setAttribute("userId", userId);
+        HttpSession existingSession = request.getSession(false);
 
-        return ResponseEntity.ok(ApiResponse.success());
+        if (existingSession != null) {
+            existingSession.invalidate();
+        }
+
+        SessionDto sessionDto = authService.authenticate(loginReqDto.getEmail(),
+            loginReqDto.getPassword());
+
+        HttpSession newSession = request.getSession(true);
+
+        newSession.setAttribute("sessionDto", sessionDto);
+
+        return ApiResponse.success();
 
     }
 }
