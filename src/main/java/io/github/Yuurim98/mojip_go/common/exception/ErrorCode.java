99fbe98c8644597b240f0729ce3ff15e86a523fd
Yuurim98 <148259495+Yuurim98@users.noreply.github.com>
2025-06-16 12:26:59 +0900

    회원가입 기능 구현 (#6)
    회원가입 기능 구현

--- src/main/java/io/github/Yuurim98/mojip_go/common/exception/ErrorCode.java
+++ src/main/java/io/github/Yuurim98/mojip_go/common/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/io/github/Yuurim98/mojip_go/user/dto/UserRegisterReqDto.java
@@ -0,0 +1,25 @@
+package io.github.Yuurim98.mojip_go.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class UserRegisterReqDto {
+
+    @Size(min = 3, max = 20)
+    @NotBlank
+    private String nickname;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 15)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,15}$",
+        message = "최소 하나의 영문과 하나의 숫자를 포함하는 8~15자를 입력하세요.")
+    private String password;
+}
